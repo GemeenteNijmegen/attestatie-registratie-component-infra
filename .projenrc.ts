6c4c3ba48e3c234e65f26ed326aeb462dd69fafa
Marnix Dessing <m.dessing@nijmegen.nl>
2026-02-02 11:33:53 +0100

    chore: fix github release and builds

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,6 +4,14 @@ const project = new GemeenteNijmegenCdkApp({
   defaultReleaseBranch: 'main',
   name: 'attestatie-registratie-component-infra',
   projenrcTs: true,
+  releaseWorkflowEnv: {
+    VER_ID_GH_TOKEN: '${{ secrets.VER_ID_GH_TOKEN }}',
+  },
+  buildWorkflowOptions: {
+    env: {
+      VER_ID_GH_TOKEN: '${{ secrets.VER_ID_GH_TOKEN }}',
+    },
+  },
   deps: [
     '@gemeentenijmegen/projen-project-type',
     '@gemeentenijmegen/attestatie-registratie-component',
