92235cba2244d0b9f2b96994ccec1a8e1fdd3520
Esper Kuijs <esper_kuijs@hotmail.com>
2026-02-24 10:33:27 +0100

    chore: cache node modules in pipeline to improve performance

--- src/PipelineStack.ts
+++ src/PipelineStack.ts
@@ -1,7 +1,7 @@
 import { PermissionsBoundaryAspect } from '@gemeentenijmegen/aws-constructs';
 import { getNodeVersion } from '@gemeentenijmegen/projen-project-type';
 import { Aspects, CfnParameter, Stack, StackProps, pipelines } from 'aws-cdk-lib';
-import { BuildSpec } from 'aws-cdk-lib/aws-codebuild';
+import { BuildSpec, Cache, LocalCacheMode } from 'aws-cdk-lib/aws-codebuild';
 import { PipelineType } from 'aws-cdk-lib/aws-codepipeline';
 import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
@@ -78,6 +78,7 @@ export class PipelineStack extends Stack {
       synth: synthStep,
       pipelineType: PipelineType.V1,
       synthCodeBuildDefaults: {
+        cache: Cache.local(LocalCacheMode.CUSTOM),
         partialBuildSpec: BuildSpec.fromObject({
           phases: {
             install: {
@@ -85,6 +86,9 @@ export class PipelineStack extends Stack {
                 nodejs: getNodeVersion(),
               },
             },
+          },
+          cache: {
+            paths: ['node_modules/**/*'],
           },
         }),
       },
