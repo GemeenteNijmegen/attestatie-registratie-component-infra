9f5d6584dbd768d9b9daf847b07a0366a481baec
Marnix Dessing <m.dessing@nijmegen.nl>
2026-03-19 16:00:05 +0100

    chore: upgrade dependencies (new ARC version)

--- src/AttestatieRegistratieComponent/lambda/arc.lambda.ts
+++ src/AttestatieRegistratieComponent/lambda/arc.lambda.ts
@@ -1,10 +1,10 @@
-import { randomUUID } from 'crypto';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 import { AttestatieRegestratieComponent, ProductenService, VerIdAttestationService, VerIdAttestationServiceConfig } from '@gemeentenijmegen/attestatie-registratie-component';
 import { AWS } from '@gemeentenijmegen/utils';
 import { DynamoDBCacheManager } from '@ver-id/node-client';
 import { ALBEvent, ALBResult } from 'aws-lambda';
+import { randomUUID } from 'crypto';
 
 const dynamoClient = DynamoDBDocumentClient.from(
   new DynamoDBClient({}),
@@ -33,7 +33,7 @@ export async function handler(event: ALBEvent): Promise<ALBResult> {
         cacheManager: dynamoDbCacheManager,
       } as VerIdAttestationServiceConfig),
       productenService: new ProductenService(),
-      // apiKey: await AWS.getSecret(process.env.ARC_API_KEY_ARN!),
+      apiKey: await AWS.getSecret(process.env.ARC_API_KEY_ARN!),
     });
 
     if (event.path.includes('/start')) {
