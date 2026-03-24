3bd5341ae8b76322fa0abc59c754149874221228
Marnix Dessing <m.dessing@nijmegen.nl>
2026-03-24 11:17:41 +0100

    chore: upgrade arc

--- src/AttestatieRegistratieComponent/lambda/arc.lambda.ts
+++ src/AttestatieRegistratieComponent/lambda/arc.lambda.ts
@@ -1,6 +1,6 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
-import { AttestatieRegistratieComponent, OpenProductApiService, VerIdAttestationService } from '@gemeentenijmegen/attestatie-registratie-component';
+import { ARC, DynamoDb, OpenProduct, OpenProductStandplaatsvergunning, VerID } from '@gemeentenijmegen/attestatie-registratie-component';
 import { AWS } from '@gemeentenijmegen/utils';
 import { DynamoDBCacheManager } from '@ver-id/node-client';
 import { ALBResult, LambdaFunctionURLEvent } from 'aws-lambda';
@@ -35,33 +35,27 @@ export async function handler(event: LambdaFunctionURLEvent): Promise<ALBResult>
   const request = parseEvent(event);
 
   try {
-    const dynamoDbCacheManager = new DynamoDBCacheManager({
-      client: dynamoClient,
-      tableName: process.env.CACHE_TABLE_NAME!,
-      options: {
-        ttlSeconds: 600,
-      },
-    });
-    const arc = new AttestatieRegistratieComponent({
-      attestationService: new VerIdAttestationService({
-        client_secret: await AWS.getSecret(process.env.VERID_CLIENT_SECRET!),
-        issuerUri: process.env.VERID_ISSUER_URL!,
-        redirectUri: process.env.ARC_CALLBACK_ENDPOINT!,
-        cacheManager: dynamoDbCacheManager,
-      }),
-      productenService: new OpenProductApiService({
-        apiToken: await AWS.getSecret(process.env.OPEN_PRODUCT_API_KEY!),
-        baseUrl: process.env.OPEN_PRODUCT_BASE_URL!,
-      }),
-      apiKey: await AWS.getSecret(process.env.ARC_API_KEY_ARN!),
-    });
+
+    // const arc = new AttestatieRegistratieComponent({
+    //   attestationService: new VerIdAttestationService({
+    //     client_secret: await AWS.getSecret(process.env.VERID_CLIENT_SECRET!),
+    //     issuerUri: process.env.VERID_ISSUER_URL!,
+    //     redirectUri: process.env.ARC_CALLBACK_ENDPOINT!,
+    //     cacheManager: dynamoDbCacheManager,
+    //   }),
+    //   productenService: new OpenProductApiService({
+    //     apiToken: await AWS.getSecret(process.env.OPEN_PRODUCT_API_KEY!),
+    //     baseUrl: process.env.OPEN_PRODUCT_BASE_URL!,
+    //   }),
+    //   apiKey: await AWS.getSecret(process.env.ARC_API_KEY_ARN!),
+    // });
 
     if (request.path.includes('/start')) {
-      return await start(request, arc);
+      return await start(request);
     };
 
     if (request.path.includes('/callback')) {
-      return await callback(request, arc);
+      return await callback(request);
     };
 
   } catch (error) {
@@ -84,38 +78,104 @@ export async function handler(event: LambdaFunctionURLEvent): Promise<ALBResult>
 
 }
 
-
-async function start(request: ArcRequest, arc: AttestatieRegistratieComponent): Promise<ALBResult> {
+/**
+ * Starts the issueance process
+ * @param request
+ * @param arc
+ * @returns
+ */
+async function start(request: ArcRequest): Promise<ALBResult> {
   console.log('Handling start...', request);
+
+  // Secure this endpoint
+  const apikey = await AWS.getSecret(process.env.ARC_API_KEY_ARN!);
+  if (!apikey || request.authorization !== apikey) {
+    throw Error('Unauthorized');
+  }
 
   if (request.type != 'producten' || !request.authorization) {
     throw Error('Invalid request');
   }
 
-  const redirectUri = await arc.start({
+  const arc = await createARC();
+
+  const result = await arc.issue({
+    source: 'openproduct', //TODO map from request.type
     id: request.productId,
-    type: request.type,
-    token: request.authorization,
+    attestation: 'standplaatsvergunning',
   });
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ url: redirectUri }),
+    body: JSON.stringify({ url: result.url }),
     headers: {
       'Content-Type': 'application/json',
     },
   };
 }
 
-async function callback(request: ArcRequest, arc: AttestatieRegistratieComponent): Promise<ALBResult> {
+/**
+ * Redirects the user to the portal's result page after callback.
+ * @param request
+ * @param arc
+ * @returns
+ */
+async function callback(request: ArcRequest): Promise<ALBResult> {
   console.log('Handling callback...', request);
 
-  const success = await arc.callback(request);
+  const prarams = new URLSearchParams({
+    is_wallet_ingeladen: 'true',
+    status: 'true',
+  });
 
   return {
     statusCode: 302,
     headers: {
-      Location: `https://mijn.dev.nijmegen.nl/producten?is_wallet_ingeladen=true&status=${success}`,
+      Location: `https://mijn.dev.nijmegen.nl/producten?${prarams.toString()}`,
     },
   };
+}
+
+
+async function createARC() {
+  const dynamoDbCacheManager = new DynamoDBCacheManager({
+    client: dynamoClient,
+    tableName: process.env.CACHE_TABLE_NAME!,
+    options: {
+      ttlSeconds: 600,
+    },
+  });
+  const arc = new ARC({
+    provider: new VerID(
+      {
+        issuerUri: process.env.VERID_ISSUER_URL!,
+        redirectUri: process.env.ARC_CALLBACK_ENDPOINT!,
+        clientSecret: await AWS.getSecret(process.env.VERID_CLIENT_SECRET!),
+        cacheManager: dynamoDbCacheManager,
+      },
+      {
+        standplaatsvergunning: {
+          flowUuid: 'xxx-xxx-xxx-xxx',
+        },
+        overkleidingsakte: {
+          flowUuid: 'xxx-xxx-xxx-xxx',
+        },
+      },
+    ),
+    store: new DynamoDb({
+      tableName: process.env.ARC_STATE_TABLE!,
+      defaultTtlSeconds: 3600,
+    }),
+    sources: [
+      new OpenProduct({
+        baseUrl: process.env.OPEN_PRODUCT_BASE_URL!,
+        apiToken: await AWS.getSecret(process.env.OPEN_PRODUCT_API_KEY!),
+      }),
+    ],
+    attestations: [
+      new OpenProductStandplaatsvergunning(),
+    ],
+  });
+
+  return arc;
 }
